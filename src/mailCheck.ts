a5cc9f99ccfb3cb68215265ac6f5d5680bf9bf8c
Emile Emredeleu <emileemre.deleu@student.vinci.be>
2026-01-16 17:34:44 +0100

    first functionnality test passes

--- src/mailCheck.ts
+++ src/mailCheck.ts
@@ -1,3 +1,9 @@
 export function validMailName(mail :string) : boolean {
+     for(let i =0; i<mail.length  ;i++){
+        if (mail[i]=='@')
+            return true;
+
+
+    }
     return false;
 }
